f1496d481df1f40698e13e6aff35adf918149aa5
root <root@ip-172-31-18-200.ec2.internal>
2025-09-01 15:28:25 +0000

    This is my 2nd commit

--- file.java
+++ file.java
@@ -1,6 +1,3 @@
-import java.util.Scanner;  
-import java.lang.Math;  
-import java.io.printer; 
 import java.io.collection; 
 import java.io.scanner;
 import java.io.outputprinter;
@@ -34,4 +31,9 @@ System.out.println();
 //function calling  
 printPattern(n);  
 }  
-}  
+} for(int j = 1; j <= size; j++)
+{
+//calculates and prints the values for pattern
+System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");
+}
+ 
